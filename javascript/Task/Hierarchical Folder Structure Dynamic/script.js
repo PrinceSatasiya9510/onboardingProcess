465deced357203dfcb4af48f68ca8f57d327a843
satasiyaprince9510 <satasiyaprince9510@gmail.com>
2025-09-22 09:07:21 +0530

    file moving

--- javascript/Task/Hierarchical Folder Structure Dynamic/script.js
+++ javascript/Task/Hierarchical Folder Structure Dynamic/script.js
@@ -230,7 +230,7 @@ document.addEventListener("dragstart", function (e) {
   }
 });
 
-document.addEventListener("dragend", function (e) {
+document.addEventListener("dragend", function () {
   if (movingFolder) {
     movingFolder = null;
   }
@@ -244,16 +244,13 @@ document.addEventListener("dragover", function (e) {
 document.addEventListener("drop", function (e) {
   e.preventDefault();
   const currentFolder = e.target.closest(".Folder");
-  if (!movingFolder || !currentFolder || movingFolder === currentFolder) return;
 
   const movingId = Number(movingFolder.getAttribute("data-id"));
   const targetId = Number(currentFolder.getAttribute("data-id"));
 
-  if (movingId === targetId) return;
 
   const movingObj = findByID(movingId, myFolderData);
   const targetObj = findByID(targetId, myFolderData);
-  if (!movingObj || !targetObj) return;
 
   const oldParent = findByID(movingObj.parentId, myFolderData);
   if (oldParent) {
