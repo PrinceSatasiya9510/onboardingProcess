b269953ab9e1f032a2fe6fec2fa3ac71dfd82887
PrinceSatasiya9510 <satasiyaprince9510@gmail.com>
2025-11-03 11:11:46 +0530

    Delete Nest js/learning/src directory

--- Nest js/learning/src/auth/get-user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { UserEntity } from "./user.entity";
-
-export const GetUser = createParamDecorator((_data, context: ExecutionContext): UserEntity => {
-    const req = context.switchToHttp().getRequest();
-    return req.user;
-})
--- Nest js/learning/src/auth/users.repository.ts
+++
@@ -1,4 +0,0 @@
-import { Repository } from "typeorm";
-import { UserEntity } from "./user.entity";
-
-export type UsersRepository = Repository<UserEntity>;
